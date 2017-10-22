f04c576744a289ab7d79d469bebc6a1fbf62050d
jGold6001 <jgold6001@gmail.com>
2017-10-22 13:31:18 +0300

    create OneToMany

---
+++ OneToManyHibernateRelationship/src/main/java/org/itstep/java_tests/OneToManyHibernateRelationship/App.java
@@ -0,0 +1,13 @@
+package org.itstep.java_tests.OneToManyHibernateRelationship;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
